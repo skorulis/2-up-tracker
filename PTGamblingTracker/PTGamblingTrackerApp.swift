fe3ece96ac6b2fe5a27f5190ea13275781d47854
Alex Skorulis <skorulis@gmail.com>
2022-04-23 10:23:21 +1000

    Initial Commit

---
+++ PTGamblingTracker/PTGamblingTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PTGamblingTrackerApp.swift
+//  PTGamblingTracker
+//
+//  Created by Alexander Skorulis on 23/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct PTGamblingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
