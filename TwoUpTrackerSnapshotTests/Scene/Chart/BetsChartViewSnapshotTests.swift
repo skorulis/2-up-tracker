2116f1c0259ce0b08d53ab6e4f87f36f420f2f79
Alex Skorulis <skorulis@gmail.com>
2023-04-07 12:21:20 +1000

    Get some basic snapshots working

--- TwoUpTrackerSnapshotTests/Scene/Chart/BetsChartViewSnapshotTests.swift
+++ TwoUpTrackerSnapshotTests/Scene/Chart/BetsChartViewSnapshotTests.swift
@@ -9,8 +9,23 @@ final class BetsChartViewSnapshotTests: XCTestCase {
     
     private let ioc = IOC()
     
-    func test_emptyChart() {
+    func test_previews() {
         let view = BetsChartView_Previews.previews(ioc: ioc)
+        assertSnapshot(view: view)
+    }
+    
+    func test_filled() {
+        let store = ioc.resolve(MainStore.self)!
+        store.bets = [
+            .init(time: 1, amount: 50),
+            .init(time: 100, amount: 20),
+            .init(time: 200, amount: 50),
+            .init(time: 400, amount: -50),
+            .init(time: 600, amount: -40),
+            .init(time: 1000, amount: -40),
+        ]
+        
+        let view = BetsChartView(viewModel: self.ioc.resolve())
         assertSnapshot(view: view)
     }
     
